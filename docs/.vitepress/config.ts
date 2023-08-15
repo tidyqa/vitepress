99253ccc2f49943f5c4b53c0f31eebca2e702f47
tidyqa <42070055+tidyqa@users.noreply.github.com>
2023-08-15 18:05:00 +0200

    1 file modified

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
       { rel: 'icon', type: 'image/svg+xml', href: '/files/favicon.svg' },
     ],
     ['link', { rel: 'icon', type: 'image/png', href: '/files/favicon.png' }],
-    ['script', { src: '/snippets/urlModalWidget.js' }],
+    ['link', { rel: 'prefetch', href: '/snippets/urlModalWidget.js' }],
+    //['script', { src: '/snippets/urlModalWidget.js' }],
   ],
   //cleanUrls: true,
   themeConfig: {
